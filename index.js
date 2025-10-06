1b3a9057aa472d2c48fcd70d383abd96258c2eb2
janethcpti24-glitch <janethcp.ti24@utsjr.edu.mx>
2025-10-05 21:11:54 -0600

    Agregar nuevas vistas y modificar archivos existentes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express"
 import rutas from "./routes/rutas.js"
-const app=express()
-app.set("view engine", "ejs")
-app.use("/",rutas)
 
-const PORT=process.env.PORT||3000
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+app.use("/", rutas)
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, function(){
-console.log("Aplicacion en http://localhost" +PORT)
+	console.log("Aplicacion en http://localhost:"+PORT)
 })
--- routes/rutas.js
+++ routes/rutas.js
@@ -1,16 +1,31 @@
-import {Router} from "express"
-const router=Router()
+import { Router } from "express"
+const router = Router()
+
+var artistas = ["Taylor Swift", "Doja Cat", "Bad Bunny", "Melanie Martinez"]
 
 router.get("/", function(req, res){
-	res.render("home")
+    res.render("home", {artistas})
 })
 
 router.get("/info", function(req, res){
-	res.render("info")
+    res.render("info", {dato: null})
 })
 
-router.get("/contactanos", function(req, res){
-	res.render("contactanos")
+router.get("/info/:dato", function(req, res){
+    var dato = req.params.dato
+    res.render("info", {dato})
 })
+
+router.get("/contacto", function(req, res){
+    res.render("contactanos")
+})
+
+router.post("/contacto", function(req, res){
+    var nombre = req.body.nombre
+    var edad = req.body.edad
+    console.log("Nombre:" + nombre + " Edad:" + edad)
+    res.render("respuesta", {nombre, edad})
+})
+
 
 export default router
